303af1a83ab0923e12eba21fe74bf1b0d58fe15d
NicolaLombardiContrader <39057248+NicolaLombardiContrader@users.noreply.github.com>
2019-02-19 16:57:01 +0100

    fix manageUser

--- wmesjsp/src/main/java/wmes/servlet/UserServlet.java
+++ wmesjsp/src/main/java/wmes/servlet/UserServlet.java
@@ -17,7 +17,7 @@ import wmes.service.UserServiceDTO;
 
 
 /**
- * La servle t si occupa di parlare con la JSP e utilizza i servizi opportuni.
+ * La servlet si occupa di parlare con la JSP e utilizza i servizi opportuni.
  * Per chi far� User dovr� anche occuparsi del Login che abbiamo lasciato come struttura e va modificata in modo opportuno
  *
  */
