becaf7d71f61f3324c18bb13f30edbdf22703524
NicolaLombardiContrader <39057248+NicolaLombardiContrader@users.noreply.github.com>
2019-02-22 11:38:37 +0100

    Add files via upload

---
+++ wmes-spring/src/main/java/it/contrader/wmesspring/WmesSpringApplication.java
@@ -0,0 +1,13 @@
+package it.contrader.wmesspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WmesSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WmesSpringApplication.class, args);
+	}
+
+}
---
+++ wmes-spring/src/test/java/it/contrader/wmesspring/WmesSpringApplicationTests.java
@@ -0,0 +1,16 @@
+package it.contrader.wmesspring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WmesSpringApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
