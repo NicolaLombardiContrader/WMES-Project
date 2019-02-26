fe5552310d1a1df561a23a18558251a347cf877e
Emilia <e.salerno@contrader.it>
2019-02-26 16:17:09 +0100

    Terminato ClientService

--- wmes-spring/src/main/java/it/contrader/wmesspring/service/ClientService.java
+++ wmes-spring/src/main/java/it/contrader/wmesspring/service/ClientService.java
@@ -1,5 +1,66 @@
 package it.contrader.wmesspring.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import it.contrader.wmesspring.converter.ConverterClient;
+import it.contrader.wmesspring.converter.ConverterUser;
+import it.contrader.wmesspring.dao.ClientRepository;
+import it.contrader.wmesspring.dto.ClientDTO;
+import it.contrader.wmesspring.dto.UserDTO;
+import it.contrader.wmesspring.model.Client;
+import it.contrader.wmesspring.model.User;
+
 public class ClientService {
 
+	private final ClientRepository clientRepository;
+	
+	public ClientService(ClientRepository clientRepository) {
+		this.clientRepository = clientRepository;
+	}
+	
+	public List<ClientDTO> getListaClientDTO(){
+		return ConverterClient.toListDTO((List<Client>) clientRepository.findAll());
+		
+	}
+	
+	public ClientDTO getClientDTOById(Integer id) {
+		return ConverterClient.toDTO(clientRepository.findById(id).get());
+	}
+
+	
+
+	public boolean insertClient(ClientDTO clientDTO) {
+		return clientRepository.save(ConverterClient.toEntity(clientDTO)) != null;
+	}
+	
+	public boolean updateClient(ClientDTO clientDTO) {
+		return clientRepository.save(ConverterClient.toEntity(clientDTO)) != null;
+	}
+	
+	public void deleteClientById(Integer id) {
+		clientRepository.deleteById(id);
+	}
+	
+	
+	
+	public List<ClientDTO> findClientDTOByUser( UserDTO userDTO) {
+		
+		final List<Client> listClient = clientRepository.findAllByUser(ConverterUser.toEntity(userDTO));
+		final List<ClientDTO> listClientDTO = new ArrayList<>();
+		listClient.forEach(i -> listClientDTO.add(ConverterClient.toDTO(i)));
+		return listClientDTO;
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
