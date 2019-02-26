dd80947cf7253a35ba0e322ac3994e72280fef8d
Emilia <e.salerno@contrader.it>
2019-02-26 11:54:04 +0100

    Aggiunta ConvertClient

---
+++ wmes-spring/src/main/java/it/contrader/wmesspring/converter/ConverterClient.java
@@ -0,0 +1,66 @@
+package it.contrader.wmesspring.converter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import it.contrader.wmesspring.dto.ClientDTO;
+import it.contrader.wmesspring.dto.UserDTO;
+import it.contrader.wmesspring.model.Client;
+import it.contrader.wmesspring.model.User;
+
+
+
+public class ConverterClient {
+	
+	public static ClientDTO toDTO(Client client) {
+
+			ClientDTO clientDTO = null;
+			
+			if (client != null) {
+				clientDTO = new ClientDTO();
+				UserDTO userDTO = ConverterUser.toDTO(client.getUser());
+								
+				clientDTO.setClientName(client.getClientName());
+
+				clientDTO.setClientId(client.getClientId());	
+			}
+		
+		return clientDTO;
+	}
+	
+	public static Client toEntity(ClientDTO clientDTO) {
+		
+		Client client = null;
+		
+		if (clientDTO != null) {
+			client = new Client();
+			User user = ConverterUser.toEntity(clientDTO.getUserDTO());
+
+			client.setClientId(clientDTO.getClientId());
+			client.setClientName(clientDTO.getClientName());
+		}
+		return client;	
+		}
+	
+	public static List<ClientDTO> toListDTO(List<Client> list) {
+		List<ClientDTO> listClientDTO = new ArrayList<>();
+		if (!list.isEmpty()) {
+			for (Client client : list) {
+				listClientDTO.add(ConverterClient.toDTO(client));
+			}
+		}
+		return listClientDTO;
+	}
+
+	public static List<Client> toListEntity(List<ClientDTO> listClientDTO) {
+		List<Client> list = new ArrayList<>();
+		if (!listClientDTO.isEmpty()) {
+			for (ClientDTO clientDTO : listClientDTO) {
+				list.add(ConverterClient.toEntity(clientDTO));
+			}
+		}
+		return list;
+	}
+	}
+	
+	
