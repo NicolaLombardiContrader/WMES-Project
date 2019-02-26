47255124c37759273d62dfc294e4fb1135761c88
Emilia <e.salerno@contrader.it>
2019-02-26 16:43:24 +0100

    terminato OrderService

---
+++ wmes-spring/src/main/java/it/contrader/wmesspring/service/OrderService.java
@@ -0,0 +1,56 @@
+package it.contrader.wmesspring.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import it.contrader.wmesspring.converter.ConverterClient;
+import it.contrader.wmesspring.converter.ConverterOrder;
+import it.contrader.wmesspring.converter.ConverterUser;
+import it.contrader.wmesspring.dao.OrderRepository;
+import it.contrader.wmesspring.dto.ClientDTO;
+import it.contrader.wmesspring.dto.OrderDTO;
+import it.contrader.wmesspring.dto.UserDTO;
+import it.contrader.wmesspring.model.Client;
+import it.contrader.wmesspring.model.Order;
+
+
+public class OrderService {
+
+private final OrderRepository orderRepository;
+	
+	public OrderService(OrderRepository orderRepository) {
+		this.orderRepository= orderRepository;
+	}
+	
+	public List<OrderDTO> getListOrderDTO(){
+		return ConverterOrder.toListDTO((List<Order>) orderRepository.findAll());
+	}
+	
+	public OrderDTO getOrderDTOById(Integer id) {
+		return ConverterOrder.toDTO(orderRepository.findById(id).get());
+	}
+
+	
+
+	public boolean insertOrder(OrderDTO orderDTO) {
+		return orderRepository.save(ConverterOrder.toEntity(orderDTO)) != null;
+	}
+	
+	public boolean updateOrder(OrderDTO orderDTO) {
+		return orderRepository.save(ConverterOrder.toEntity(orderDTO)) != null;
+	}
+	
+	public void deleteOrderById(Integer id) {
+		orderRepository.deleteById(id);
+	}
+	
+	
+	
+	public List<OrderDTO> findClientDTOByUser( UserDTO userDTO) {
+		
+		final List<Order> listOrder = orderRepository.findAllByUser(ConverterUser.toEntity(userDTO));
+		final List<OrderDTO> listOrderDTO = new ArrayList<>();
+		listOrder.forEach(i -> listOrderDTO.add(ConverterOrder.toDTO(i)));
+		return listOrderDTO;
+	}
+}
