5403f8dc3454b4dd05013d9f022d0041d335660a
Fabio <f.giancaspro@contrader.it>
2019-02-27 16:07:43 +0100

    meglio tardi che mai

--- wmes-spring/src/main/java/it/contrader/wmesspring/controller/ClientController.java
+++ wmes-spring/src/main/java/it/contrader/wmesspring/controller/ClientController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import it.contrader.wmesspring.dto.ClientDTO;
+import it.contrader.wmesspring.dto.OrderDTO;
 import it.contrader.wmesspring.dto.UserDTO;
 import it.contrader.wmesspring.service.ClientService;
 
@@ -48,39 +49,56 @@ public class ClientController {
 
 	}
 
-	@RequestMapping(value = "/crea", method = RequestMethod.GET)
+	@RequestMapping(value = "/insertRedirect", method = RequestMethod.GET)
 	public String insert(HttpServletRequest request) {
-		visualClient(request);
-		request.setAttribute("option", "insert");
-		return "creaClient";
+		return "client/insertClient";
 	}
-/*
-	@RequestMapping(value = "/cercaClient", method = RequestMethod.GET)
-	public String cercaClient(HttpServletRequest request) {
 
-		final String content = request.getParameter("search");
-
-		List<ClientDTO> allClient = this.clientService.findClientDTOByUser(userDTO)(content);
-		request.setAttribute("allClientDTO", allClient);
-
-		return "homeClient";
-
-	}
-	*/
-
-	@RequestMapping(value = "/creaClient", method = RequestMethod.POST)
+	@RequestMapping(value = "/insert", method = RequestMethod.POST)
 	public String insertClient(HttpServletRequest request) {
-		String UserId = request.getParameter("UserId").toString();
-		String ClientName = request.getParameter("ClientName").toString();
 		UserDTO userLogged = (UserDTO) session.getAttribute("utente");
 		
+		String clientName = request.getParameter("client_name").toString();
+		
+
 		ClientDTO clientObj = new ClientDTO();
+		clientObj.setClientName(clientName);
 		clientObj.setUserDTO(userLogged);
-		clientObj.setClientName(ClientName);
 		clientService.insertClient(clientObj);
-
 		visualClient(request);
-		return "homeClient";
-	}
+		 
 
+		return "client/insertClient";
+	}
+	@RequestMapping(value = "/updateRedirect", method = RequestMethod.GET)
+	public String updateRedirect(HttpServletRequest request) {
+		int id = Integer.parseInt(request.getParameter("id"));
+		ClientDTO clientUpdate = new ClientDTO();
+
+		clientUpdate = this.clientService.getClientDTOById(id);
+		request.setAttribute("clientUpdate", clientUpdate);
+		return "client/updateClient";
+	}		
+	
+
+	@RequestMapping(value = "/update", method = RequestMethod.GET)
+	public String update(HttpServletRequest request) {
+		
+		Integer idUpdate = Integer.parseInt(request.getParameter("order_id"));
+
+		UserDTO userLogged = (UserDTO) session.getAttribute("utente");
+
+		String clientName = request.getParameter("client_name").toString();
+
+		
+		ClientDTO client = new ClientDTO();
+		client.setClientName(clientName);
+		client.setUserDTO(userLogged);
+		
+		clientService.updateClient(client);
+		visualClient(request);
+		
+		
+		return "client/manageClientclientclient";
+	}
 }
