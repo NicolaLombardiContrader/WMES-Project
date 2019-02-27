3371f56a3eb3fc944f5f19b2ad0c08bf4cf0247d
Artem contrader <a.matyushin@contrader.it>
2019-02-27 15:41:59 +0100

    project controller need task list

--- wmes-spring/src/main/java/it/contrader/wmesspring/controller/ProjectController.java
+++ wmes-spring/src/main/java/it/contrader/wmesspring/controller/ProjectController.java
@@ -11,41 +11,31 @@ import it.contrader.wmesspring.dto.ProjectDTO;
 import it.contrader.wmesspring.dto.UserDTO;
 import it.contrader.wmesspring.service.ProjectService;
 
-
 @Controller
 
 @RequestMapping("/Project")
 public class ProjectController {
-	
+
 	private final ProjectService projectService;
 	private HttpSession session;
-	
-	
+
 	@Autowired
 	public ProjectController(ProjectService projectService) {
 		this.projectService = projectService;
 	}
 
-	
 	private void visualProject(HttpServletRequest request) {
 		List<ProjectDTO> allProject = this.projectService.getListProjectDTO();
 		request.setAttribute("allProjectDTO", allProject);
 	}
-	
-	
-	
-	
-	
+
 	@RequestMapping(value = "/projectManagement", method = RequestMethod.GET)
 	public String prjectManagement(HttpServletRequest request) {
 		visualProject(request);
 		return "homeProject";
 	}
-	
-	
-	
-	
-	//Delete
+
+	// Delete
 	@RequestMapping(value = "/delete", method = RequestMethod.GET)
 	public String delete(HttpServletRequest request) {
 		int id = Integer.parseInt(request.getParameter("id"));
@@ -55,31 +45,49 @@ public class ProjectController {
 		return "homeProject";
 
 	}
-	
-	
-		@RequestMapping(value = "/creaProject", method = RequestMethod.POST)
-		public String insertProject(HttpServletRequest request) {
-			UserDTO userLogged = (UserDTO) session.getAttribute("utente");
-			String projectname = request.getParameter("projectname").toString();
-			int projectStatus = 0;
-			
 
-			
-			ProjectDTO projectObj = new ProjectDTO();
-			projectObj.setProjectName(projectname);
-			projectObj.setProjectStatus(projectStatus);
-			projectObj.setUserDTO(userLogged);
-			projectObj.setProjectStatus(0);
-			projectService.insertProject(projectObj);
+	@RequestMapping(value = "/updateRedirect", method = RequestMethod.GET)
+	public String updateRedirect(HttpServletRequest request) {
+		int id = Integer.parseInt(request.getParameter("id"));
+		ProjectDTO projectUpdate = new ProjectDTO();
+		projectUpdate = this.projectService.getProjectDTOById(id);
+		request.setAttribute("projectUpdate", projectUpdate);
+		return "project/updateProject";
+	}
 
-			visualProject(request);
-			return "homeProject";
-		}
+	@RequestMapping(value = "/update", method = RequestMethod.GET)
+	public String update(HttpServletRequest request) {
+		Integer idProject = Integer.parseInt(request.getParameter("project_id"));
+		String projectNameUpdate = request.getParameter("project_name");
+		int projectStatusUpdate = Integer.parseInt(request.getParameter("project_status"));
+		UserDTO userLogged = (UserDTO) session.getAttribute("utente");
 
+		ProjectDTO project = new ProjectDTO();
+		project.setUserDTO(userLogged);
+
+		project.setProjectName(projectNameUpdate);
+		project.setProjectStatus(projectStatusUpdate);
+		project.setProjectId(idProject);
+//TODO list task
+		projectService.updateProject(project);
+		visualProject(request);
+		return "project/manageProject";
+	}
+
+	@RequestMapping(value = "/insert", method = RequestMethod.POST)
+	public String insertProject(HttpServletRequest request) {
+		UserDTO userLogged = (UserDTO) session.getAttribute("utente");
+		String projectname = request.getParameter("project_name").toString();
+		ProjectDTO projectObj = new ProjectDTO();
+		projectObj.setProjectName(projectname);
+		projectObj.setUserDTO(userLogged);
+		projectObj.setProjectStatus(0);
+		projectService.insertProject(projectObj);
+
+		visualProject(request);
+		return "project/manageProject";
 		
-		}
+	}
 	
-	
-	
-	
-	
+
+}
