5ec88c1f7c34fa2cb8467c5b2fe7827820995adf
Teresa <t.martorelli@contrader.it>
2019-03-05 17:40:24 +0100

    questo

--- src/main/java/it/contrader/wmesspring/controller/TaskController.java
+++ src/main/java/it/contrader/wmesspring/controller/TaskController.java
@@ -1,11 +1,6 @@
 package it.contrader.wmesspring.controller;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -24,15 +19,9 @@ import java.util.List;
 public class TaskController {
 
 	private final TaskService taskService;
-	private final ResourceService resourceService;
-	
-	@Autowired
-	private HttpSession session;
-
 	@Autowired
 	public TaskController(TaskService taskService, ResourceService resourceService) {
 		this.taskService = taskService;
-		this.resourceService=resourceService;
 	}
 
 	/*
