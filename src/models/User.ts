a21577c80b9312bf3dc8c1984b49987f1fab2f2b
Contrader102 <Contrader102@DESKTOP-40MIQJP>
2019-03-05 16:19:55 +0100

    User service

---
+++ src/models/User.ts
@@ -0,0 +1,14 @@
+export class User {
+    useruser: string;
+    userpass: string;
+    usertype: number;
+
+
+    constructor(useruser: string, userpass: string, usertype: number) {
+        this.useruser = useruser;
+        this.userpass = userpass;
+        this.usertype = usertype;
+
+    }
+    
+}
