c0985d8723b5cdf99ca07a5502f7324fcc96ad11
Contrader102 <Contrader102@DESKTOP-40MIQJP>
2019-03-06 16:39:43 +0100

    new routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,11 +5,36 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 /** Home */
 import { HomeAdminComponent } from './components/home-admin/home-admin.component';
+import { HomeBoComponent } from './components/home-bo/home-bo.component';
+import { HomeResourceComponent } from './components/home-resource/home-resource.component';
+import { UserManagementComponent } from './components/user/user-management/user-management.component';
+import { UserInsertComponent } from './components/user/user-insert/user-insert.component';
+import { UserUpdateComponent } from './components/user/user-update/user-update.component';
+import { ClientManagementComponent } from './components/client/client-management/client-management.component';
+import { ClientInsertComponent } from './components/client/client-insert/client-insert.component';
+import { ClientUpdateComponent } from './components/client/client-update/client-update.component';
+import { OrderManagementComponent } from './components/order/order-management/order-management.component';
+import { OrderInsertComponent } from './components/order/order-insert/order-insert.component';
+import { OrderUpdateComponent } from './components/order/order-update/order-update.component';
 
 const routes: Routes = [
     { path: '', redirectTo: '/login', pathMatch: 'full' },
     { path: 'login', component: LoginComponent },
-    { path: 'homeAdmin', component: HomeAdminComponent}
+    { path: 'homeAdmin', component: HomeAdminComponent },
+    { path: 'homeBO', component: HomeBoComponent },
+    { path: 'homeResource', component: HomeResourceComponent },
+    /** User */
+    { path: 'User/userManagement', component: UserManagementComponent },
+    { path: 'User/insert', component: UserInsertComponent },
+    { path: 'User/update', component: UserUpdateComponent },
+    /** Client */
+    { path: 'Client/clientManagement', component: ClientManagementComponent },
+    { path: 'Client/insert', component: ClientInsertComponent },
+    { path: 'Client/update', component: ClientUpdateComponent },
+    /** Order */
+    { path: 'Order/orderManagement', component: OrderManagementComponent },
+    { path: 'Order/insert', component: OrderInsertComponent },
+    { path: 'Order/update', component: OrderUpdateComponent },
 ];
 
 @NgModule({
