b56948ed7dad0fcf44e07695e3fdcd3fda6d35e5
NicolaLombardiContrader <39057248+NicolaLombardiContrader@users.noreply.github.com>
2019-03-07 10:20:44 +0100

    login in unser service

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { logging } from 'protractor';
-import { environment } from '../../environments/environment.prod';
+import { environment } from '../models/environment.models';
 import { tap, catchError } from 'rxjs/operators';
 import { HttpClient } from '@angular/common/http';
 import { User } from '../models/User';
@@ -13,7 +13,7 @@ import { Observable, of, BehaviorSubject } from 'rxjs';
     providedIn: 'root'
 })
 export class UserService {
-    feedback: string;
+ private urlBase = environment.url;
 
     constructor(private http: HttpClient) { }
 
@@ -25,25 +25,26 @@ export class UserService {
         };
     }
 
+    isLogged(): boolean {
+        if (typeof (Storage) !== 'undefined') {
+            if (sessionStorage.getItem('user') != null) {
+                return true;
+            }
+        // tslint:disable-next-line:align
+        } return false;
+    }
     login(username: string, password: string): Observable<User> {
-        return this.http.get<User>('http://localhost:58708/api/login?username=' + username + '&password=' + password)
+        return this.http.get<User>('http://localhost:8080/user/UserController=' + username + '&password=' + password)
             .pipe(tap((response) => console.log('User'), catchError(this.handleError('login error', {})))
             );
     }
 
-    signup(user: User): Observable<boolean> {
-        return this.http.post<boolean>('http://localhost:58708/api/signupUser', user)
-            .pipe(tap((response) => console.log('User'), catchError(this.handleError('login error', {})))
-            );
+logOut() {
+    if (typeof (Storage) !== 'undefined') {
+      sessionStorage.removeItem('user');
     }
+  }
 
-    changeFeedback(message: string) {
-        this.feedback = message;
-    }
-
-    deleteFeedback() {
-        this.feedback = '';
-    }
 
 }
 
