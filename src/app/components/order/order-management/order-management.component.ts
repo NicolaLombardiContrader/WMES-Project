bae08469f2cddde39a7dc2811f06c505c1e1c503
franc <franc@LAPTOP-KQ96TE6D>
2019-03-07 17:50:37 +0100

    orderManagement

--- src/app/components/order/order-management/order-management.component.ts
+++ src/app/components/order/order-management/order-management.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { OrderService } from '../../../services/order.service';
+import { Order } from '../../../models/Order';
 
 @Component({
-  selector: 'app-order-management',
-  templateUrl: './order-management.component.html',
-  styleUrls: ['./order-management.component.css']
+    selector: 'app-order-management',
+    templateUrl: './task-order.component.html',
+    styleUrls: ['./task-order.component.css']
 })
 export class OrderManagementComponent implements OnInit {
+    public orders: Array<Order>;
 
-  constructor() { }
+    constructor(private orderService: OrderService, private router: Router) { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this.orderService.orderList().subscribe((response) => {
+            this.orders = response;
+            console.log('La grandezza e\'' + this.orders.length);
+        });
+    }
 
+    removeLink(orderId: number) {
+        /** this.router.navigateByUrl('/delete?taskId=' + taskId); */
+        this.orderService.deleteOrder(orderId);
+        /** this.router.navigateByUrl('/Task/taskManagement'); */
+    }
+
+    updateLink(orderId: string) {
+        this.router.navigateByUrl('/update?orderId=' + orderId);
+    }
 }
+
--- src/app/services/order.service.ts
+++ src/app/services/order.service.ts
@@ -3,11 +3,14 @@ import { logging } from 'protractor';
 import { environment } from '../../environments/environment.prod';
 import { tap, catchError } from 'rxjs/operators';
 import { HttpClient } from '@angular/common/http';
-import { Order } from '../models/Order';
+import { Task } from '../models/Task';
 import { Observable, of, BehaviorSubject } from 'rxjs';
-
-
-
+import { Resource } from '../models/Resource';
+import { Project } from '../models/Project';
+import { ProjectTemplate } from '../models/ProjectTemplate';
+import { User } from '../models/User';
+import { Order } from '../models/Order';
+import { Client } from '../models/Client';
 
 @Injectable({
     providedIn: 'root'
@@ -24,18 +27,31 @@ export class OrderService {
             return of(result as T);
         };
     }
-
-
-    changeFeedback(message: string) {
-        this.feedback = message;
+    orderList(): Observable<Array<Order>> {
+        const user: User = JSON.parse(sessionStorage.getItem('user'));
+        return this.http.get<any>('http://localhost:8080/Order/orderManagement?userId=' + user.userId)
+            .pipe(tap((response) => console.log('Order'), catchError(this.handleError('error', {})))
+            );
     }
 
-    deleteFeedback() {
-        this.feedback = '';
+
+    // tslint:disable-next-line:max-line-length
+    insertOrder(orderId: number, orderDescription: string , user: User, client: Client): Observable<Order> {
+        // tslint:disable-next-line:prefer-const
+        // tslint:disable-next-line:max-line-length
+        const userInsert: User = JSON.parse(sessionStorage.getItem('user'));
+        const resourceInsert: Resource = JSON.parse(sessionStorage.getItem('resource'));
+        // tslint:disable-next-line:max-line-length
+        const newOrder = new Order(0, orderDescription, user, client);
+        return this.http.post<Order>('http://localhost:8080/Order/insert', newOrder)
+            .pipe(tap((response) => console.log('insertOrder'), catchError(this.handleError('insertOrder error', {})))
+            );
+    }
+
+    deleteOrder(orderId: number) {
+        // this.http.get<any>('http://localhost:8080/Order/delete?orderId=' + orderId)
+        //    .pipe(tap((response) => console.log('Order'), catchError(this.handleError('delete Order error', {}))));
+       this.http.get('http://localhost:8080/Order/delete?orderId=' + orderId);
     }
 
 }
-
-
-
-
