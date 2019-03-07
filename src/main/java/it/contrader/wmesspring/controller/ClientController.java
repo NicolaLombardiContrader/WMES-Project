143a828402c56ac1bd6283292b5eb586c7738a23
Teresa <t.martorelli@contrader.it>
2019-03-07 14:35:26 +0100

    LUCE DEI MIEI OCCHI

--- src/main/java/it/contrader/wmesspring/controller/ClientController.java
+++ src/main/java/it/contrader/wmesspring/controller/ClientController.java
@@ -1,6 +1,7 @@
 package it.contrader.wmesspring.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,6 +13,7 @@ import it.contrader.wmesspring.service.ClientService;
 
 import java.util.List;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/Client")
 public class ClientController {
