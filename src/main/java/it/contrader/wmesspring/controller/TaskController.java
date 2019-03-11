5535b8161b6c76664b843cd0f85a59d9bac77fa8
Contrader102 <Contrader102@DESKTOP-40MIQJP>
2019-03-11 14:37:26 +0100

    taskcontrooler

--- src/main/java/it/contrader/wmesspring/controller/TaskController.java
+++ src/main/java/it/contrader/wmesspring/controller/TaskController.java
@@ -2,6 +2,7 @@ package it.contrader.wmesspring.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -35,21 +36,18 @@ public class TaskController {
 		userDTOTaskList.setUserId(userId);
 		return this.taskService.findTaskDTOByUser(ConverterUser.toEntity(userDTOTaskList));
 	}
-	
+
 	@RequestMapping(value = "/delete", method = RequestMethod.DELETE)
 	public void delete(@RequestParam(value = "taskId") int taskId) {
 		this.taskService.deleteTaskById(taskId);
 	}
-
-
 
 	@RequestMapping(value = "/insert", method = RequestMethod.POST)
 	public List<TaskDTO> insert(@RequestParam(value = "userDTO") UserDTO userLogged,
 			@RequestParam(value = "task_action") String taskAction,
 			@RequestParam(value = "task_description") String taskDescription,
 			@RequestParam(value = "task_input") String taskInput,
-			@RequestParam(value = "task_output") String taskOutput,
-			@RequestParam(value = "task_time") int taskTime,
+			@RequestParam(value = "task_output") String taskOutput, @RequestParam(value = "task_time") int taskTime,
 			@RequestParam(value = "task_state") int taskState,
 			@RequestParam(value = "resource_id") ResourceDTO resourceId) {
 
@@ -75,30 +73,39 @@ public class TaskController {
 
 	}
 
+	/*
+	 * @RequestMapping(value = "/update", method = RequestMethod.POST) public
+	 * List<TaskDTO> update(@RequestParam(value = "task_id") int idUpdate,
+	 * 
+	 * @RequestParam(value = "utente") UserDTO userLogged,
+	 * 
+	 * @RequestParam(value = "task_action") String actionUpdate,
+	 * 
+	 * @RequestParam(value = "task_description") String descriptionUpdate,
+	 * 
+	 * @RequestParam(value = "task_input") String inputUpdate,
+	 * 
+	 * @RequestParam(value = "task_output") String outputUpdate,
+	 * 
+	 * @RequestParam(value = "task_state") int stateUpdate, @RequestParam(value =
+	 * "task_time") int timeUpdate,
+	 * 
+	 * @RequestParam(value = "resource_id") ResourceDTO resourceUpdateId) {
+	 * 
+	 * TaskDTO task = new TaskDTO(); task.setTaskId(idUpdate);
+	 * task.setTaskAction(actionUpdate); task.setTaskDescription(descriptionUpdate);
+	 * task.setTaskInput(inputUpdate); task.setTaskOutput(outputUpdate);
+	 * task.setTaskState(stateUpdate); task.setTaskTime(timeUpdate);
+	 * task.setResourceDTO(resourceUpdateId); task.setUserDTO(userLogged);
+	 * 
+	 * taskService.updateTask(task); return this.taskService.getListaTaskDTO();
+	 * 
+	 * }
+	 */
+
 	@RequestMapping(value = "/update", method = RequestMethod.POST)
-	public List<TaskDTO> update(@RequestParam(value = "task_id") int idUpdate,
-			@RequestParam(value = "utente") UserDTO userLogged,
-			@RequestParam(value = "task_action") String actionUpdate,
-			@RequestParam(value = "task_description") String descriptionUpdate,
-			@RequestParam(value = "task_input") String inputUpdate,
-			@RequestParam(value = "task_output") String outputUpdate,
-			@RequestParam(value = "task_state") int stateUpdate, @RequestParam(value = "task_time") int timeUpdate,
-			@RequestParam(value = "resource_id") ResourceDTO resourceUpdateId) {
-
-		TaskDTO task = new TaskDTO();
-		task.setTaskId(idUpdate);
-		task.setTaskAction(actionUpdate);
-		task.setTaskDescription(descriptionUpdate);
-		task.setTaskInput(inputUpdate);
-		task.setTaskOutput(outputUpdate);
-		task.setTaskState(stateUpdate);
-		task.setTaskTime(timeUpdate);
-		task.setResourceDTO(resourceUpdateId);
-		task.setUserDTO(userLogged);
-
+	public void update(@RequestBody TaskDTO task) {
 		taskService.updateTask(task);
-		return this.taskService.getListaTaskDTO();
-
 	}
 
 }
