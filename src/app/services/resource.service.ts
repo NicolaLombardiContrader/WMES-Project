c1a8faa6d7cb111a92521bececa850e062ed0622
NicolaLombardiContrader <39057248+NicolaLombardiContrader@users.noreply.github.com>
2019-03-11 10:05:08 +0100

    resource

--- src/app/services/resource.service.ts
+++ src/app/services/resource.service.ts
@@ -6,6 +6,7 @@ import { HttpClient } from '@angular/common/http';
 import { Resource } from '../models/Resource';
 import { Observable, of, BehaviorSubject } from 'rxjs';
 import { User } from '../models/User';
+import { Task } from '../models/Task';
 
 
 
@@ -32,6 +33,37 @@ export class ResourceService {
             .pipe(tap((response) => console.log('Resource'), catchError(this.handleError('error', {})))
             );
     }
+
+
+    // tslint:disable-next-line:max-line-length
+    insertResource(resourceId: number, resourceName: string, resourceUsername: string, resourcePass: string, user: User, task: Task[]): Observable<Resource> {
+        // tslint:disable-next-line:prefer-const
+        // tslint:disable-next-line:max-line-length
+        const userInsert: User = JSON.parse(sessionStorage.getItem('user'));
+        // tslint:disable-next-line:max-line-length
+        const newTask = new Resource(0, resourceName, resourceUsername, resourcePass, user, task);
+        return this.http.post<Resource>('http://localhost:8080/Resource/insert', newTask)
+            .pipe(tap((response) => console.log('insertResource'), catchError(this.handleError('insertResource error', {})))
+            );
+    }
+
+  readResource(resourceId: number): Observable<Resource> {
+        return this.http.get<any>('http://localhost:8080/Resource/read?taskId=' + resourceId)
+            .pipe(tap((response) => console.log('Resource'), catchError(this.handleError('error', {})))
+            );
+    }
+
+ deleteResource(resourceId: number) {
+        // this.http.get<any>('http://localhost:8080/Task/delete?taskId=' + taskId)
+        //    .pipe(tap((response) => console.log('Task'), catchError(this.handleError('delete Task error', {}))));
+        this.http.delete('http://localhost:8080/Task/delete?taskId=' + resourceId).subscribe(() => console.log('Resource deleted'));
+    }
+
+updateResource(resource: Resource): void {
+        this.http.put('http://localhost:8080/Resource/update', resource).subscribe(() => console.log('Resource updated'));
+    }
+
+
 
 }
 
