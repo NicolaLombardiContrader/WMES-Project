c404ad5d96cb13f21455bfedb6ea5a63b4d1624a
Emilia <e.salerno@contrader.it>
2019-03-12 10:01:52 +0100

    project

--- src/app/components/project/project-management/project-management.component.ts
+++ src/app/components/project/project-management/project-management.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ProjectService } from '../../../services/project.service';
+import { Project } from '../../../models/Project';
 
 @Component({
-  selector: 'app-project-management',
-  templateUrl: './project-management.component.html',
-  styleUrls: ['./project-management.component.css']
+    selector: 'app-project-management',
+    templateUrl: './project-management.component.html',
+    styleUrls: ['./project-management.component.css']
 })
 export class ProjectManagementComponent implements OnInit {
 
-  constructor() { }
+    public projects: Array<Project>;
+    constructor(private projectService: ProjectService, private router: Router) { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this.projectService.projectList().subscribe((response) => {
+            this.projects = response;
+            console.log('La grandezza e\'' + this.projects.length);
+        });
+    }
 
+    removeLink(projectId: number) {
+        this.projectService.deleteProject(projectId);
+    }
+
+    updateLink(projectId: number) {
+        this.router.navigateByUrl('/Project/update/' + projectId);
+    }
+
+    insertLink() {
+        this.router.navigateByUrl('/Project/insert');
+    }
 }
+
