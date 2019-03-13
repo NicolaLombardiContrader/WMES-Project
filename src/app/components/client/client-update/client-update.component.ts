fc7ac7c7cf1bee23b018734bec60cca531c7b158
Emilia <e.salerno@contrader.it>
2019-03-13 09:51:25 +0100

    client update completato lettura completata solo da fare insert

--- src/app/components/client/client-update/client-update.component.ts
+++ src/app/components/client/client-update/client-update.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { NgForm } from '@angular/forms';
+import { User } from '../../../models/User';
+import { ClientService } from '../../../services/client.service';
+import { Client } from '../../../models/Client';
+
 
 @Component({
   selector: 'app-client-update',
@@ -7,9 +13,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClientUpdateComponent implements OnInit {
 
-  constructor() { }
+    clientId: number;
+    clientName: string;
+    public client: Client;
+
+  constructor(private clientService: ClientService, private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
-  }
+      this.clientId = Number(this.route.snapshot.paramMap.get('clientId'));
+      console.log('Resource id in update:' + this.clientId);
+      this.clientService.readClient(this.clientId).subscribe((response) => {
+          this.client = response;
+          console.log('Client caricato: ' + this.client.clientName);
+      });
+    }
+
+   update(f: NgForm) {
+        console.log(f.value);
+        const userUpdate: User = JSON.parse(sessionStorage.getItem('user'));
+        this.client.userDTO = userUpdate;
+        this.clientService.updateClient(this.client);
+     //   this.router.navigateByUrl('/Client/clientManagement');
+    }
 
 }
