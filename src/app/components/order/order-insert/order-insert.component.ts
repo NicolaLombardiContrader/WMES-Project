203251f28fba86097dd69cd88224159241d95fc6
franc <franc@LAPTOP-KQ96TE6D>
2019-03-13 10:12:03 +0100

    order component

--- src/app/components/order/order-insert/order-insert.component.ts
+++ src/app/components/order/order-insert/order-insert.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { OrderService } from '../../../services/order.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { User } from '../../../models/User';
+import { Order } from '../../../models/Order';
+import { Client } from '../../../models/Client';
+import { NgForm } from '@angular/forms';
 
 @Component({
-  selector: 'app-order-insert',
-  templateUrl: './order-insert.component.html',
-  styleUrls: ['./order-insert.component.css']
+    selector: 'app-order-insert',
+    templateUrl: './order-insert.component.html',
+    styleUrls: ['./order-insert.component.css']
 })
 export class OrderInsertComponent implements OnInit {
 
-  constructor() { }
+    public order: Order;
+    constructor(private orderService: OrderService, private router: Router) { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        const userInsert: User = JSON.parse(sessionStorage.getItem('user'));
+        
+        /*COVERTER ENTITY->DTO ?? (UserDTO)*/
+        this.order = new Order(null, null, userInsert, null);
+    }
+
+    insertOrder(f: NgForm) {
+        console.log('Order id: ' + f.value.orderId + ' ' + f.value.orderDescription);
+        this.orderService.insertOrder(this.order);
+    }
 
 }
