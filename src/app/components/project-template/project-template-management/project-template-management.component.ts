b5ea0d8c8d5b1f15cc43bd0fa54327bf6e0999c8
Emilia <e.salerno@contrader.it>
2019-03-14 10:30:28 +0100

    os

--- src/app/components/project-template/project-template-management/project-template-management.component.ts
+++ src/app/components/project-template/project-template-management/project-template-management.component.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import { ProjectTemplate } from '../../../models/ProjectTemplate';
+import { ProjectTemplateService } from '../../../services/projectTemplate.service';
+import { Router } from '@angular/router';
+import { subscribeOn } from 'rxjs/operators';
+
 
 @Component({
-  selector: 'app-project-template-management',
-  templateUrl: './project-template-management.component.html',
-  styleUrls: ['./project-template-management.component.css']
+    selector: 'app-project-template-management',
+    templateUrl: './project-template-management.component.html',
+    styleUrls: ['./project-template-management.component.css']
 })
 export class ProjectTemplateManagementComponent implements OnInit {
 
-  constructor() { }
+        public projectsTemplate: Array<ProjectTemplate>;
 
-  ngOnInit() {
-  }
+    constructor(private projectTemplateService: ProjectTemplate, private router: Router) { }
+
+    ngOnInit() {
+        this.projectTemplateService.projectTemplateList().subscribe((response) => {
+            this.projectsTemplate = response;
+            console.log('La grandezza e\'' + this.projectsTemplate.length);
+        });
+    }
+
+    removeLink(projectTemplateId: number) {
+        this.projectTemplateService.deleteProjectTemplate(projectTemplateId);
+    }
+
+    updateLink(projectTemplateId: number) {
+        this.router.navigateByUrl('/ProjectTemplate/update/' + projectTemplateId);
+    }
+
+    insertLink() {
+        this.router.navigateByUrl('/ProjectTemplate/insert');
+    }
+    cloneLink() {
+        this.router.navigateByUrl('/ProjectTemplate/clone');
+    }
 
 }
--- src/app/services/projectTemplate.service.ts
+++ src/app/services/projectTemplate.service.ts
@@ -5,7 +5,8 @@ import { tap, catchError } from 'rxjs/operators';
 import { HttpClient } from '@angular/common/http';
 import { ProjectTemplate } from '../models/ProjectTemplate';
 import { Observable, of, BehaviorSubject } from 'rxjs';
-
+import { User } from '../models/User';
+import { Task } from '../models/Task';
 
 
 
@@ -25,17 +26,34 @@ export class ProjectTemplateService {
         };
     }
 
-
-    changeFeedback(message: string) {
-        this.feedback = message;
+    projectTemplateList(): Observable<Array<ProjectTemplate>> {
+        const user: User = JSON.parse(sessionStorage.getItem('user'));
+        return this.http.get<any>('http://localhost:8080/ProjectTemplate/projectTemplateManagement?userId=' + user.userId)
+            .pipe(tap((response) => console.log('ProjectTemplate'), catchError(this.handleError('error', {})))
+            );
     }
 
-    deleteFeedback() {
-        this.feedback = '';
+    insertProjectTemplate(projectTemplate: ProjectTemplate): void {
+        this.http.post('http://localhost:8080/ProjectTemplate/insert', projectTemplate).subscribe(() => console.log('ProjectTemplate'));
     }
+
+    readProjectTemplate(projectTemplateId: number): Observable<ProjectTemplate> {
+        return this.http.get<any>('http://localhost:8080/ProjectTemplate/read?projectTemplateId=' + projectTemplateId)
+            .pipe(tap((response) => console.log('ProjectTemplate'), catchError(this.handleError('error', {})))
+            );
+    }
+
+    deleteProjectTemplate(projectTemplateId: number) {
+        this.http.delete('http://localhost:8080/ProjectTemplate/delete?projectTemplateId=' + projectTemplateId).subscribe(() =>
+            console.log('ProjectTemplate deleted'));
+    }
+
+    updateProjectTemplate(projectTemplate: ProjectTemplate): void {
+        this.http.put('http://localhost:8080/ProjectTemplate/update', projectTemplate).subscribe(() =>
+            console.log('ProjectTemplate updated'));
+    }
+
+
+
 
 }
-
-
-
-
