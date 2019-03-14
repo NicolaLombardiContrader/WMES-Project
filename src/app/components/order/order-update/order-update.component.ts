c4c3ba8363360c0e55921b479d19084eb1dea440
Artem contrader <a.matyushin@contrader.it>
2019-03-14 17:17:36 +0100

    dddd

--- src/app/components/order/order-update/order-update.component.ts
+++ src/app/components/order/order-update/order-update.component.ts
@@ -5,37 +5,45 @@ import { Client } from '../../../models/Client';
 import { Order } from '../../../models/Order';
 import { NgForm } from '@angular/forms';
 import { User } from '../../../models/User';
+import { ClientService } from '../../../services/client.service';
 
 
 @Component({
-  selector: 'app-order-update',
-  templateUrl: './order-update.component.html',
-  styleUrls: ['./order-update.component.css']
+    selector: 'app-order-update',
+    templateUrl: './order-update.component.html',
+    styleUrls: ['./order-update.component.css']
 })
 export class OrderUpdateComponent implements OnInit {
+    public orderUpdate: Order;
     orderId: number;
     clientName: string;
     orderDescription: string;
-    public order: Order;
-
+    public clientsUpdate: Array<Client>;
     // tslint:disable-next-line:max-line-length
-    constructor(private orderService: OrderService, private route: ActivatedRoute, private router: Router) { }
+    constructor(private orderService: OrderService, private clientService: ClientService, private route: ActivatedRoute, private router: Router) { }
 
     ngOnInit() {
+
+        this.clientService.clientList().subscribe((response) => {
+            this.clientsUpdate = response;
+            console.log('Lista clienti caricarita');
+        });
+
         /** Convert String to number */
         this.orderId = Number(this.route.snapshot.paramMap.get('orderId'));
         console.log('Order id in update:' + this.orderId);
         this.orderService.readOrder(this.orderId).subscribe((response) => {
-            this.order = response;
-            console.log('Order caricarito: ' + this.order.orderId);
+            this.orderUpdate = response;
+            console.log('Order caricarito: ' + this.orderUpdate.orderId);
         });
+
     }
 
     update(f: NgForm) {
         console.log(f.value);
         const userUpdate: User = JSON.parse(sessionStorage.getItem('user'));
-        this.order.userDTO = userUpdate;
-        this.orderService.updateOrder(this.order);
+        this.orderUpdate.userDTO = userUpdate;
+        this.orderService.updateOrder(this.orderUpdate);
     }
 
 }
