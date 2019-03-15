15fb7b4746c70144179d2ea09b4886077f38fdd6
NicolaLombardiContrader <39057248+NicolaLombardiContrader@users.noreply.github.com>
2019-03-15 15:19:56 +0100

    brberb

--- src/main/java/it/contrader/wmesspring/controller/TaskModelController.java
+++ src/main/java/it/contrader/wmesspring/controller/TaskModelController.java
@@ -14,48 +14,46 @@ import it.contrader.wmesspring.service.ResourceService;
 import it.contrader.wmesspring.service.TaskModelService;
 import java.util.List;
 
-	@CrossOrigin
-	@RestController
-	@RequestMapping("/TaskModel")
-	public class TaskModelController {
+@CrossOrigin
+@RestController
+@RequestMapping("/TaskModel")
+public class TaskModelController {
 
-		private final TaskModelService taskModelService;
+	private final TaskModelService taskModelService;
 
-		@Autowired
-		public TaskModelController(TaskModelService taskModelService, ResourceService resourceService) {
-			this.taskModelService = taskModelService;
-		}
+	@Autowired
+	public TaskModelController(TaskModelService taskModelService, ResourceService resourceService) {
+		this.taskModelService = taskModelService;
+	}
 
-		@RequestMapping(value = "/taskModelManagement", method = RequestMethod.GET)
-		public List<TaskModelDTO> taskModelManagement(@RequestParam(value = "userId") int userId) {
-			UserDTO userDTOTaskModelList = new UserDTO();
-			userDTOTaskModelList.setUserId(userId);
-			return this.taskModelService.findTaskModelDTOByUser(ConverterUser.toEntity(userDTOTaskModelList));
-		}
+	@RequestMapping(value = "/taskModelManagement", method = RequestMethod.GET)
+	public List<TaskModelDTO> taskModelManagement(@RequestParam(value = "userId") int userId) {
+		UserDTO userDTOTaskModelList = new UserDTO();
+		userDTOTaskModelList.setUserId(userId);
+		return this.taskModelService.findTaskModelDTOByUser(ConverterUser.toEntity(userDTOTaskModelList));
+	}
 
-		@RequestMapping(value = "/delete", method = RequestMethod.DELETE)
-		public void delete(@RequestParam(value = "taskModelId") int taskModelId) {
-			this.taskModelService.deleteTaskModelById(taskModelId);
-		}
+	@RequestMapping(value = "/delete", method = RequestMethod.DELETE)
+	public void delete(@RequestParam(value = "taskModelId") int taskModelId) {
+		this.taskModelService.deleteTaskModelById(taskModelId);
+	}
 
-		@RequestMapping(value = "/insert", method = RequestMethod.POST)
-		public void insert(@RequestBody TaskModelDTO taskModel) {
-			taskModelService.insertTaskModel(taskModel);
-		}
+	@RequestMapping(value = "/insert", method = RequestMethod.POST)
+	public void insert(@RequestBody TaskModelDTO taskModel) {
+		taskModelService.insertTaskModel(taskModel);
+	}
 
-
-		@RequestMapping(value = "/read", method = RequestMethod.GET)
-		public TaskModelDTO read(@RequestParam(value = "taskModelId") int id) {
-			TaskModelDTO taskModelUpdate = new TaskModelDTO();
-			taskModelUpdate = this.taskModelService.getTaskModelDTOById(id);
-			return taskModelUpdate;
-
-		}
-
-		@RequestMapping(value = "/update", method = RequestMethod.PUT)
-		public void update(@RequestBody TaskModelDTO taskModel) {
-			taskModelService.updateTaskModel(taskModel);
-		}
+	@RequestMapping(value = "/read", method = RequestMethod.GET)
+	public TaskModelDTO read(@RequestParam(value = "taskModelId") int id) {
+		TaskModelDTO taskModelUpdate = new TaskModelDTO();
+		taskModelUpdate = this.taskModelService.getTaskModelDTOById(id);
+		return taskModelUpdate;
 
 	}
-	
+
+	@RequestMapping(value = "/update", method = RequestMethod.PUT)
+	public void update(@RequestBody TaskModelDTO taskModel) {
+		taskModelService.updateTaskModel(taskModel);
+	}
+
+}
