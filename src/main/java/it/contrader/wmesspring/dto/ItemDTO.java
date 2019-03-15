c4b438ab1ed730393cd1e3cd88d145b1c6e5a322
Contrader102 <Contrader102@DESKTOP-40MIQJP>
2019-03-15 12:57:50 +0100

    task DTO

---
+++ src/main/java/it/contrader/wmesspring/dto/ItemDTO.java
@@ -0,0 +1,30 @@
+package it.contrader.wmesspring.dto;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import it.contrader.wmesspring.model.Task;
+import it.contrader.wmesspring.model.User;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ItemDTO {
+
+	private int itemId;
+
+	private String itemCode;
+
+	private String itemName;
+
+	private String itemType;
+
+	private UserDTO userDTO;
+
+}
--- src/main/java/it/contrader/wmesspring/dto/TaskDTO.java
+++ src/main/java/it/contrader/wmesspring/dto/TaskDTO.java
@@ -7,34 +7,45 @@ import lombok.NoArgsConstructor;
 
 import java.util.List;
 
+import javax.persistence.Column;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import it.contrader.wmesspring.model.Item;
+import it.contrader.wmesspring.model.Project;
+import it.contrader.wmesspring.model.Resource;
+import it.contrader.wmesspring.model.Task;
+import it.contrader.wmesspring.model.User;
+
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class TaskDTO {
-
 	
 	private int taskId;
 
 	private String taskAction;
-
+	
 	private String taskDescription;
-
-	private String taskInput;
-
-	private String taskOutput;
-
+	
 	private int taskState;
 
 	private int taskTime;
-
-	private List<ProjectTemplateDTO> projectTemplatesDTO;
-
-	private List<ProjectDTO> projectsDTO;
+	
+	private List<ItemDTO> itemsDTO;
 
 	private ResourceDTO resourceDTO;
 
+	private ProjectDTO projectDTO;
+
 	private UserDTO userDTO;
 
+	private List<TaskDTO> childsListDTO;
 
 }
