287f7d5912a6c785434b753b21b81e34b052af6c
NicolaLombardiContrader <39057248+NicolaLombardiContrader@users.noreply.github.com>
2019-03-15 14:59:33 +0100

    rfrf

--- src/main/java/it/contrader/wmesspring/service/TaskModelService.java
+++ src/main/java/it/contrader/wmesspring/service/TaskModelService.java
@@ -15,44 +15,45 @@ import it.contrader.wmesspring.model.User;
 
 public class TaskModelService {
 
-	private final TaskModelRepository taskModelRepository; 
-	
+	private final TaskModelRepository taskModelRepository;
+
 	@Autowired
 	public TaskModelService(TaskModelRepository taskModelRepository) {
 		this.taskModelRepository = taskModelRepository;
 	}
-	
+
 	public List<TaskModelDTO> getListaTaskModelDTO() {
 		return ConverterTaskModel.toListDTO((List<TaskModel>) taskModelRepository.findAll());
 	}
-	
+
 	public TaskModelDTO getTaskModelDTOById(Integer id) {
 		return ConverterTaskModel.toDTO(taskModelRepository.findById(id).get());
 	}
-	
+
 	public List<TaskModelDTO> getListaTaskModelDTOByUser(UserDTO user) {
-		return ConverterTaskModel.toListDTO((List<TaskModel>) taskModelRepository.findAllByUser(ConverterUser.toEntity(user)));
-	
+		return ConverterTaskModel
+				.toListDTO((List<TaskModel>) taskModelRepository.findAllByUser(ConverterUser.toEntity(user)));
+
 	}
-	
+
 	public boolean insertTaskModel(TaskModelDTO taskModelDTO) {
 		return taskModelRepository.save(ConverterTaskModel.toEntity(taskModelDTO)) != null;
 	}
-	
+
 	public boolean updateTaskModel(TaskModelDTO taskModelDTO) {
 		return taskModelRepository.save(ConverterTaskModel.toEntity(taskModelDTO)) != null;
 	}
-	
+
 	public void deleteTaskModelById(Integer id) {
 		taskModelRepository.deleteById(id);
 	}
 
-	public List<TaskModelDTO> findTaskModelDTOByUser( User user) {
-		
+	public List<TaskModelDTO> findTaskModelDTOByUser(User user) {
+
 		final List<TaskModel> listTaskModel = taskModelRepository.findAllByUser((user));
 		final List<TaskModelDTO> listTaskModelDTO = new ArrayList<>();
 		listTaskModel.forEach(i -> listTaskModelDTO.add(ConverterTaskModel.toDTO(i)));
 		return listTaskModelDTO;
-	
+
 	}
 }
