b544d4b3766e916808778c680d27f3c1d95a0133
Contrader102 <Contrader102@DESKTOP-40MIQJP>
2019-03-18 15:28:53 +0100

    task insert

--- src/app/components/project-tree/project-tree-insert-node/project-tree-insert-node.component.ts
+++ src/app/components/project-tree/project-tree-insert-node/project-tree-insert-node.component.ts
@@ -3,7 +3,7 @@ import { TaskModelService } from '../../../services/taskModel.service';
 import { TaskModel } from '../../../models/TaskModel';
 import { NgForm } from '@angular/forms';
 import { ProjectService } from '../../../services/project.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
     selector: 'app-project-tree-insert-node',
@@ -14,7 +14,8 @@ export class ProjectTreeInsertNodeComponent implements OnInit {
     public taskModelList: Array<TaskModel>;
     public taskFatherId = '0';
     public projectId: string;
-    constructor(private taskModelService: TaskModelService, private projectService: ProjectService, private route: ActivatedRoute) { }
+    // tslint:disable-next-line:max-line-length
+    constructor(private taskModelService: TaskModelService, private projectService: ProjectService, private route: ActivatedRoute, private router: Router) { }
 
     ngOnInit() {
         this.projectId = this.route.snapshot.paramMap.get('projectId');
@@ -33,5 +34,6 @@ export class ProjectTreeInsertNodeComponent implements OnInit {
         this.projectService.insertTaskNode(this.projectId,
             projectTreeInsertTask.value.taskModelIdSelected,
             this.taskFatherId);
+        this.router.navigateByUrl('/Project/update/' + this.projectId);
     }
 }
