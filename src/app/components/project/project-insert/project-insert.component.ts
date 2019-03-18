75321a706a3aae196783236dd3e60f23b0c1ce60
Contrader102 <Contrader102@DESKTOP-40MIQJP>
2019-03-18 12:12:19 +0100

    fixed project insert

--- src/app/components/project/project-insert/project-insert.component.ts
+++ src/app/components/project/project-insert/project-insert.component.ts
@@ -2,11 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Project } from '../../../models/Project';
 import { ProjectService } from '../../../services/project.service';
 import { User } from '../../../models/User';
-import { Task } from '../../../models/Task';
 import { NgForm } from '@angular/forms';
-import { TaskModelService } from '../../../services/taskModel.service';
-import { TaskModel } from '../../../models/TaskModel';
-import { Resource } from '../../../models/Resource';
 
 @Component({
     selector: 'app-project-insert',
@@ -15,24 +11,16 @@ import { Resource } from '../../../models/Resource';
 })
 export class ProjectInsertComponent implements OnInit {
 
-    public projectInsert: Project;
-
-    public firstTaskId: number;
-    public project: Project;
-    public taskRootSelected: string;
-    constructor(private projectService: ProjectService, private taskModelService: TaskModelService) { }
+    constructor(private projectService: ProjectService) { }
 
     ngOnInit() {
-        const userInsert: User = JSON.parse(sessionStorage.getItem('user'));
-        this.project = new Project(null, null, null, userInsert, null);
-
     }
 
     insertProject(projectInsertForm: NgForm) {
         const userInsertProject: User = JSON.parse(sessionStorage.getItem('user'));
         const project: Project = new Project(0, projectInsertForm.value.projectName, null, userInsertProject, null);
         // tslint:disable-next-line:max-line-length
-        this.projectService.insertProject(this.project);
+        this.projectService.insertProject(project);
     }
 }
 
